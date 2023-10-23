0405e1dead6a713bbc14636957defb2a6a045d38
Barinov Maksim <craftyplonkton@gmail.com>
2023-10-23 23:51:08 +0300

    debug and allowed_hosts from env

--- career_tracker/settings.py
+++ career_tracker/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='django-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default='True') == 'True'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split()
 
 
 # Application definition
